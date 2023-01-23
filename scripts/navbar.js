8f85a4a375998c005d4f3aa7c8585c5a0a40bb78
r3kashi <r3kashit@gmail.com>
2023-01-24 00:17:49 +0800

    assets

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,12 @@
-const navIcon = document.querySelector(".nav-icon");
-const nav = document.querySelector("nav");
+// const navIcon = document.querySelector(".nav-icon");
+// const nav = document.querySelector("nav");
+
+// navIcon.onclick = function () {
+//     nav.classList.toggle('show');
+// }
+
+const navIcon = document.querySelector(".navbar__toggle-button");
+const nav = document.querySelector(".navbar");
 
 navIcon.onclick = function () {
     nav.classList.toggle('show');
