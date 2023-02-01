f362a827f1ee9e1f16738c114bc92f80bb18af60
r3kashi <r3kashit@gmail.com>
2023-02-01 21:32:36 +0800

    Create analytics.js

---
+++ scripts/analytics.js
@@ -0,0 +1,49 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5'
+      ],
+      datasets: [{
+        data: [
+          120,
+          240,
+          200,
+          300,
+          390
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
