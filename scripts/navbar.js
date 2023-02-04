f478151ded8e732666f2934366afbbd4647c594e
r3kashi <r3kashit@gmail.com>
2023-02-04 21:09:31 +0800

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,52 +1,143 @@
-const searchButton = document.querySelector("nav .desktop-nav .link-search");
-const closeButton = document.querySelector(".search-container .link-close");
-const desktopNav = document.querySelector(".desktop-nav");
-const searchContainer = document.querySelector(".search-container");
-const overlay = document.querySelector(".overlay");
+// Mobile Navbar
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryHeader = document.querySelector(".primary-header");
 
-searchButton.addEventListener("click", () => {
-    desktopNav.classList.add("hide");
-    searchContainer.classList.remove("hide");
-    overlay.classList.add("show");
-})
-
-closeButton.addEventListener("click", () => {
-    desktopNav.classList.remove("hide");
-    searchContainer.classList.add("hide");
-    overlay.classList.remove("show");
-})
-
-overlay.addEventListener("click", () => {
-    desktopNav.classList.remove("hide");
-    searchContainer.classList.add("hide");
-    overlay.classList.remove("show");
-})
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible");
 
 
-// Mobile Version
+    // console.log(visibility);
+    if(visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
 
-const menuIconContainer = document.querySelector("nav .menu-icon-container");
-const navContainer = document.querySelector(".nav-container");
+}); 
 
-menuIconContainer.addEventListener("click", () => {
-    navContainer.classList.toggle("active");
-})
+// Hide the navbar when scrolling down, display when scrolling up
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if(lastScrollY < window.scrollY) {
+        // console.log("we are going down");
+      primaryHeader.classList.add("nav--hidden");
+  }else{
+        // console.log("we are going up");
+    primaryHeader.classList.remove("nav--hidden");
+  }
+    lastScrollY = window.scrollY;
+});
 
 
-const searchBar = document.querySelector(".mobile-search-container .search-bar");
-const nav = document.querySelector(".nav-container nav");
-const searchInput = document.querySelector(".mobile-search-container input");
-const cancelBtn = document.querySelector(".mobile-search-container .cancel-btn");
+// Change the appearance of navbar when scrolling down
+const header = document.querySelector(".primary-header");
+const sectionOne = document.querySelector(".hero");
 
-searchInput.addEventListener("click", () => {
-    searchBar.classList.add("active");
-    nav.classList.add("move-up");
-    desktopNav.classList.add("move-down");
-})
+const sectionOneOptions = { 
+  rootMargin: "-200px 0px 0px 0px"
+};
 
-cancelBtn.addEventListener("click", () => {
-    searchBar.classList.remove("active");
-    nav.classList.remove("move-up");
-    desktopNav.classList.remove("move-down");
-})
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav--scrolled");
+    } else {
+      header.classList.remove("nav--scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+
+//change logo on scroll
+
+// Get the logo element
+// var logo = document.getElementById("logo");
+
+// // Get the position of the logo
+// var logoPosition = logo.getBoundingClientRect().top;
+
+// // Listen for the scroll event
+// window.addEventListener("scroll", function() {
+//   // Get the current scroll position
+//   var scrollPosition = window.pageYOffset;
+
+//   // Check if the scroll position is greater than the logo position
+//   if (scrollPosition > logoPosition) {
+//     // Change the logo
+//     logo.src = "./images/crab-logo-black.png";
+//   } else {
+//     // Change back to the original logo
+//     logo.src = "./images/crab-logo-white.png";
+//   }
+// });
+
+
+// var logo = document.getElementById("logo");
+
+// // Get the section element
+// var section = document.getElementById("section-two");
+
+// // Get the position of the section
+// var sectionPosition = section.getBoundingClientRect().top;
+
+// // Listen for the scroll event
+// window.addEventListener("scroll", function() {
+//   // Get the current scroll position
+//   var scrollPosition = window.pageYOffset;
+
+//   // Check if the scroll position is within the section
+//   if (scrollPosition > sectionPosition && scrollPosition < (sectionPosition + section.offsetHeight)) {
+//     // Change the logo
+//     logo.src = "./images/crab-logo-black.png";
+//     logo.style.transition = "all 300ms ease-in";
+//   } else {
+//     // Change back to the original logo
+//     logo.src = "./images/crab-logo-white.png";
+//     logo.style.transition = "all 300ms ease-in";
+//   }
+// });
+
+
+// Get the logo element
+var logo = document.getElementById("logo");
+
+// Get the section element
+var section = document.getElementById("section-two");
+
+// Get the position of the section
+var sectionPosition = section.getBoundingClientRect().top;
+
+// Get the screen width
+var screenWidth = window.innerWidth;
+
+// Listen for the scroll event
+window.addEventListener("scroll", function() {
+  // Check the screen size
+  if (screenWidth > 768) {
+    // Get the current scroll position
+    var scrollPosition = window.pageYOffset;
+
+    // Check if the scroll position is within the section
+    if (scrollPosition > sectionPosition && scrollPosition < (sectionPosition + section.offsetHeight)) {
+      // Change the logo
+      logo.src = "./images/crab-logo-black.png";
+      logo.style.transition = "all 0.7s ease-in-out";
+    } else {
+      // Change back to the original logo
+      logo.src = "./images/crab-logo-white.png";
+      logo.style.transition = "all 0.7s ease-in-out";
+    }
+  }
+});
+
 
